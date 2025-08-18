b2b212b6bacf759828f01f2b3a3f22a8cc1566d2
mjamiu0 <mjamiu0024@gmail.com>
2025-08-18 02:22:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -76,16 +76,16 @@ if st.button('Predict Maize Yield'):
         if predicted_yield < 1000:
             yield_category = "Low Yield"
             st.warning(f'The predicted maize yield is: **{predicted_yield:.2f} Kg per ha**')
-            st.write(f"This falls into the **{yield_category}** category.")
-        elif 1000 <= predicted_yield < 4000:
+            st.write(f"**{yield_category}** ")
+        elif 1000 <= predicted_yield < 5000:
             yield_category = "Moderate Yield"
             st.info(f'The predicted maize yield is: **{predicted_yield:.2f} Kg per ha**')
-            st.write(f"This falls into the **{yield_category}** category.")
-        else: # predicted_yield >= 4000
+            st.write(f"**{yield_category}** ")
+        else: # predicted_yield >= 5000
             yield_category = "High Yield"
             st.success(f'The predicted maize yield is: **{predicted_yield:.2f} Kg per ha**')
             st.balloons()
-            st.write(f"This falls into the **{yield_category}** category.")
+            st.write(f"**{yield_category}** category")
 
 
     except Exception as e:
